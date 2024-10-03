7ab4a2f0aff5d8a251a86fb7767823afeb38c3fa
Thuraya AlWehaib <T.AlWehaib@Takamol.sa>
2024-10-03 11:15:10 +0300

    chapter 13: Handling Errors

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -31,28 +31,45 @@ function prepareInvoice(formData: FormData) {
 
 export async function createInvoice(formData: FormData) {
     const { customerId, amountInCents, status, date } = prepareInvoice(formData);
-    await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-  `;
+    try {
+        await sql`
+        INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+      `;
+
+    } catch (error) {
+        return {
+            message: 'Database Error: Failed to Create Invoice.',
+        };
+    }
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
-
 }
 
 export async function updateInvoice(id: string, formData: FormData) {
     const { customerId, amountInCents, status } = prepareInvoice(formData);
-    await sql`
+    try {
+        await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `;
-
+    } catch (error) {
+        return {
+            message: 'Database Error: Failed to Create Invoice.',
+        };
+    }
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
 }
 
 export async function deleteInvoice(id: string) {
-    await sql`DELETE FROM invoices WHERE id = ${id}`;
-    revalidatePath('/dashboard/invoices');
-  }
+    try {
+        await sql`DELETE FROM invoices WHERE id = ${id}`;
+        revalidatePath('/dashboard/invoices');
+    } catch (error) {
+        return {
+            message: 'Database Error: Failed to Create Invoice.',
+        };
+    }
+}
